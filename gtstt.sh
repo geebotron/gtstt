aace914a0a2b32dd0061fad15994f67b77b0269c
Geeb <use_the_source@users.sourceforge.net>
2014-04-22 17:02:06 +0100

    Initial commit - gthumb search tracker tags. Shell script to open image viewer based on a query to the tracker database, searching on tags, year

---
+++ gtstt.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+sql_head="SELECT nie:url(?image)
+WHERE {
+?image a nfo:Image"
+sql_tags=
+sql_date_filter=
+sql_foot="
+}
+ORDER BY DESC(nie:contentCreated(?image))"
+tags_arr=
+sql_lim=
+sql_final=
+
+while getopts t:l:y: opt
+do
+    case $opt in
+        t)  tags_arr=(${OPTARG//,/ })
+            ;;
+        l)  sql_lim=" LIMIT ${OPTARG}"
+            ;;
+        y)  sql_date_filter="
+;nie:contentCreated ?date .
+FILTER (?date >= '${OPTARG}-01-01T00:00:00'
+&& ?date <  '${OPTARG}-11-31T00:00:00')"
+            ;;
+    esac
+done
+
+shift $((OPTIND - 1))
+
+for i in "${tags_arr[@]}"; do
+    sql_tags="${sql_tags}
+;nao:hasTag [ nao:prefLabel '$i' ]"
+done
+
+sql_final="${sql_head}\
+    ${sql_tags}\
+    ${sql_date_filter}\
+    ${sql_foot}\
+    ${sql_lim}"
+
+#echo "${sql_final}"
+
+   tracker-sparql -q "${sql_final}" \
+       | grep -o 'file:///.*' \
+       | xargs gthumb
+#    | xargs feh -t -W 1200 -. --index-info %u -
